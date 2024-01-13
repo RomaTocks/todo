c911681c72acdc5964ff6b03a1c02a8c33a1a1c3
Roman <nicotocks@mail.ru>
2024-01-13 16:53:09 +0300

    Initial commit.

---
+++ src/test/java/com/tocks/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tocks.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
